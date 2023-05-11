7816c311c9738f9506e78ff6e75de6781e5f581b
Sanderson <sandersonoficial10@hotmail.com>
2023-05-10 21:29:50 -0300

    9,7%

--- 1.go
+++ 1.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"math"
+)
 
+type Circuito struct {
+	raio float64
+}
+
+func calcular(r Circuito) float64 {
+	area := r.raio * r.raio * math.Pi
+	return area
+}
+
+func main() {
+	r := Circuito{raio: 4}
+	calcular(r)
 }
